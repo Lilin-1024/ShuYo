2f9b01176b38db01e0e82c7302ab415a5f065ff8
Lilin <LingeonU@gmail.com>
2026-07-19 12:17:59 +0800

    修改服务器端口

--- lehu_client/lib/core/client_backend_constants.dart
+++ lehu_client/lib/core/client_backend_constants.dart
@@ -1,6 +1,5 @@
 class ClientBackendConstants {
   const ClientBackendConstants._();
 
-  // Update this in one place when the backend moves from IP to domain.
-  static const baseUrl = 'http://8.213.159.150';
+  static const baseUrl = 'https://api.shuyo.work';
 }
